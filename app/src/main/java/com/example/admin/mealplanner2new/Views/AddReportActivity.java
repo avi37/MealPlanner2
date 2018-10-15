ffcce0a12c1369612101941b1b5cf5033e313172
Avi Vaishnav <4337+avi37@users.noreply.github.com>
2018-10-15 11:46:18 +0530

    morning commit of 15-10-2018

--- app/src/main/java/com/example/admin/mealplanner2new/Views/AddReportActivity.java
+++ app/src/main/java/com/example/admin/mealplanner2new/Views/AddReportActivity.java
@@ -3,11 +3,22 @@ package com.example.admin.mealplanner2new.Views;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.admin.mealplanner2new.R;
 
 public class AddReportActivity extends AppCompatActivity {
+
+    Spinner spinner_reportType;
+    TextView textView_paramName, textView_yourResult;
+    RecyclerView recyclerView_dataEntries;
+    Button button_submit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +27,31 @@ public class AddReportActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        spinner_reportType = findViewById(R.id.addReport_spinner_reportType);
+        textView_paramName = findViewById(R.id.addReport_tv_paramName);
+        textView_yourResult = findViewById(R.id.addReport_tv_result);
+        recyclerView_dataEntries = findViewById(R.id.addReport_recView_dataEntries);
+        button_submit = findViewById(R.id.addReport_btn_submit);
+
+
+        button_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                methodSubmit();
+            }
+        });
+
+        getReportTypes();
+
+    }
+
+    private void methodSubmit() {
+        // Submit method
+    }
+
+    private void getReportTypes() {
+        // API Call
 
     }
 
